daeff27447e498bd77c2e60b41e167449ca3f9cb
SoftwareEngineeringWoman <mssairawalia@gmail.com>
2023-09-18 21:12:56 -0700

    Initial Commit

---
+++ Python Intro.py
@@ -0,0 +1,18 @@
+c_name="John"
+c_age=35
+print("35 years old John.")
+
+#now to make John dynamic, so that it is easy to change in a bigger data
+# John=" + c_name +",
+
+#character_name="John"
+#character_age=35
+#print("35 years old " + character_name +",  ")
+
+character_name="Saira"  #string
+character_age=35        #whole number or decimal
+is_male= True          #Boolean
+print("35 years old " + character_name +",  ")
+character_name="Saira Walia"
+print("35 years old " + character_name +",  ")
+
