ef06ddd66dac0a8328be5f3a9e2cdeb0eb832a4c
SoftwareEngineeringWoman <mssairawalia@gmail.com>
2023-09-20 16:08:38 -0700

    Initial Commit

---
+++ Madlib games.py
@@ -0,0 +1,13 @@
+#enter words and put into a story
+
+
+color =input("enter a color name: ")
+plural_noun= input("Enter a noun: ")
+celebrity =input("enter a celeb name: ")
+
+
+
+print(" Roses are " + color)
+print(plural_noun +" are blue")
+print("I love " +celebrity)
+
