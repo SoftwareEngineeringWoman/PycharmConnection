21363015e22157150443ea77fecff332b44e4526
SoftwareEngineeringWoman <mssairawalia@gmail.com>
2023-09-19 23:30:44 -0700

    Initial Commit

---
+++ Numbers.py
@@ -0,0 +1,27 @@
+print(7+8.9)
+
+print(3*4+5)          #bodmas
+print(3*(4+5))
+
+
+print(10 % 3)    #modulo gives remainder 1
+
+my_num= 10
+print(my_num)
+print(str(my_num)+ " is cool.")  #str prints string
+
+#functions
+
+#AbS absolute value
+
+my_number= -5
+print(abs(my_number))
+
+print(pow(3,2))   #3^2 power
+print(max(3,2))    #min
+print(round(3.2))
+
+
+
+from math import*
+print(sqrt(36))
