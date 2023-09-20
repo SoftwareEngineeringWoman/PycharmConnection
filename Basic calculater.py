d1ec7e7fd9a9fcc23981586499d3b1b6e57758f6
SoftwareEngineeringWoman <mssairawalia@gmail.com>
2023-09-20 16:02:48 -0700

    Initial Commit

---
+++ Basic calculater.py
@@ -0,0 +1,11 @@
+
+Num1= input("numner1: ")
+num2= input("number2: ")
+result= int(Num1) + int(num2)
+print(result)          # convert from str to number (int for whole number )
+
+#but for decimal
+Num1= input("numner1: ")
+num2= input("number2: ")
+result= float(Num1) + float(num2)
+print(result)
