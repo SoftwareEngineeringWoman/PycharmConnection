70f5c6d9031416667e45151aa88ac71627fa1977
SoftwareEngineeringWoman <mssairawalia@gmail.com>
2023-09-20 18:10:07 -0700

    Initial Commit

---
+++ Learning resources.py
@@ -0,0 +1,37 @@
+Beginer Python freecodecamp
+https://www.youtube.com/watch?v=rfscVS0vtbw&t=3s
+
+Intermediate Python freecodecamp
+https://www.youtube.com/watch?v=HGOBQPFzWKo&t=9s
+
+Advanced Python freecodecamp
+https://www.youtube.com/watch?v=1WpKsY9LBlY
+
+python practice
+https://www.hackerrank.com/interview/preparation-kits/three-month-preparation-kit/three-month-week-one/challenges
+youtube solns
+https://youtube.com/playlist?list=PL_8jNcohs27XQfEmWAHCgLFqpsNaWxUSe&si=4xGlYwO0tYeJtI-P
+
+check mosh pythn
+https://www.youtube.com/watch?v=_uQrJ0TkZlc&t=16769s
+
+code with harry python hindi for projects
+https://www.youtube.com/watch?v=gfDE2a7MKjA&t=33933s
+
+Oops freecodecamp & harry
+https://www.youtube.com/watch?v=Ej_02ICOIgs
+https://youtube.com/playlist?list=PLu0W_9lII9ahfRrhFcoB-4lpp9YaBmdCP&si=2e0nKUjonUWhTtrR
+dsa freecodecamp
+https://www.youtube.com/watch?v=8hly31xKli0
+
+Blind75 neetcode and then all levels
+https://www.youtube.com/watch?v=KLlXCFG5TnA&list=PLot-Xpze53ldVwtstag2TL4HQhAnC8ATf
+
+python projects
+
+tim
+https://www.youtube.com/watch?v=th4OBktqK1I&t=528s
+20 projects freecodecamp
+https://www.youtube.com/watch?v=pdy3nh1tn6I
+
+
---
+++ Lists.py
@@ -0,0 +1,24 @@
+#lists stores a list of info
+
+#friends= ["sam" , 2, False]
+friends=["sam","tom","ian", "polly", "trent"]
+#           0     1     2     indexes
+#           -3    -2    -1
+print(friends)
+print(friends[2])
+print(friends[-1])
+print(friends[-2])
+print(friends[-3])
+print(friends[1:])    #everything not including 1 i.e. index 0
+print(friends[1:3])
+
+#modify
+
+#friends= ["sam" , 2, False]
+friends=["sam","tom","ian", "polly", "trent"]
+friends[1]="Hen"
+print(friends)
+
+
+
+
