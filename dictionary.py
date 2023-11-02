fbc6f6d03dfb71f2dbdf8f6dcaee9582b06c5bfc
SoftwareEngineeringWoman <mssairawalia@gmail.com>
2023-11-02 10:00:31 -0700

    New

--- dictionary.py
+++ dictionary.py
@@ -3,12 +3,17 @@
 #unique keys
 #get gives a default none if invalid key
 # to replace none with other text, use comma and write it
+#normal dictionary= word and meaning ... word is key
+#below we create a dic to convert month name to shorter name
 
 MonthConversion = {
     "Jan": "January",
     "Feb": "February",
     "Mar": "March"
 }
+print(MonthConversion["Feb"])
+
 print(MonthConversion.get("Luv"))
 
-print(MonthConversion.get("Luv", "Not a valid key"))
+print(MonthConversion.get("Luv", "Not a valid key")) # this adds default value
+
